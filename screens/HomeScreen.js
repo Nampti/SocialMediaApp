121ff13153cfd92a28c7f2f512c7df4577e99022
Nampti <91663929+Nampti@users.noreply.github.com>
2024-02-17 09:15:11 +0700

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,6 +6,9 @@ import axios from 'axios';
 import "core-js/stable/atob";
 import { UserType } from "../UserContext";
 import { useFocusEffect } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 const HomeScreen = () => {
     const { userId, setUserId } = useContext(UserType);
     const [posts, setPosts] = useState([]);
@@ -37,6 +40,40 @@ const HomeScreen = () => {
     };
 
     console.log("posts", posts);
+    const handleLike = async (postId) => {
+        try {
+            const response = await axios.put(
+                `http://192.168.0.105:3000/posts/${postId}/${userId}/like`
+            );
+            const updatedPost = response.data;
+
+            const updatedPosts = posts?.map((post) =>
+                post?._id === updatedPost._id ? updatedPost : post
+            );
+
+            setPosts(updatedPosts);
+        } catch (error) {
+            console.log("Error liking the post", error);
+        }
+    };
+
+    const handleDislike = async (postId) => {
+        try {
+            const response = await axios.put(
+                `http://192.168.0.105:3000/posts/${postId}/${userId}/unlike`
+            );
+            const updatedPost = response.data;
+            // Update the posts array with the updated post
+            const updatedPosts = posts.map((post) =>
+                post._id === updatedPost._id ? updatedPost : post
+            );
+            console.log("updated ", updatedPosts)
+
+            setPosts(updatedPosts);
+        } catch (error) {
+            console.error("Error unliking post:", error);
+        }
+    };
 
     return (
         <ScrollView style={{ marginTop: 50, flex: 1, backgroundColor: "white" }}>
@@ -48,16 +85,19 @@ const HomeScreen = () => {
                     }}
                 />
             </View>
+
             <View style={{ marginTop: 20 }}>
                 {posts?.map((post) => (
-                    <View style={{
-                        padding: 15,
-                        borderColor: "#D0D0D0",
-                        borderTopWidth: 1,
-                        flexDirection: "row",
-                        gap: 10,
-                        marginVertical: 10,
-                    }}>
+                    <View
+                        style={{
+                            padding: 15,
+                            borderColor: "#D0D0D0",
+                            borderTopWidth: 1,
+                            flexDirection: "row",
+                            gap: 10,
+                            marginVertical: 10,
+                        }}
+                    >
                         <View>
                             <Image
                                 style={{
@@ -70,6 +110,46 @@ const HomeScreen = () => {
                                     uri: "https://cdn-icons-png.flaticon.com/128/149/149071.png",
                                 }}
                             />
+                        </View>
+
+                        <View>
+                            <Text
+                                style={{ fontSize: 15, fontWeight: "bold", marginBottom: 4 }}
+                            >
+                                {post?.user?.name}
+                            </Text>
+                            <Text>{post?.content}</Text>
+
+                            <View
+                                style={{
+                                    flexDirection: "row",
+                                    alignItems: "center",
+                                    gap: 10,
+                                    marginTop: 15,
+                                }}
+                            >
+                                {post?.likes?.includes(userId) ? (
+                                    <AntDesign
+                                        onPress={() => handleDislike(post?._id)}
+                                        name="heart"
+                                        size={18}
+                                        color="red"
+                                    />
+                                ) : (
+                                    <AntDesign
+                                        onPress={() => handleLike(post?._id)}
+                                        name="hearto"
+                                        size={18}
+                                        color="black"
+                                    />
+                                )}
+
+                                <FontAwesome name="comment-o" size={18} color="black" />
+                                <Ionicons name="share-social-outline" size={18} color="black" />
+                            </View>
+                            <Text style={{ marginTop: 7, color: "gray" }}>
+                                {post?.likes?.length} likes • {post?.replies?.length} reply
+                            </Text>
                         </View>
                     </View>
                 ))}
