696adc3b8495a29f660ce515d5cd4ccc931c75e2
Nampti <91663929+Nampti@users.noreply.github.com>
2024-02-17 09:14:55 +0700

    Update ThreadsScreen.js

--- screens/ThreadsScreen.js
+++ screens/ThreadsScreen.js
@@ -4,9 +4,27 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import "core-js/stable/atob";
 import { UserType } from "../UserContext";
 import axios from 'axios';
+import { useNavigation } from "@react-navigation/native";
 const ThreadsScreen = () => {
     const { userId, setUserId } = useContext(UserType);
     const [content, setContent] = useState("");
+    const [user, setUser] = useState("");
+    const navigation = useNavigation();
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const response = await axios.get(
+                    `http://192.168.0.105:3000/profile/${userId}`
+                );
+                const { user } = response.data;
+                setUser(user);
+            } catch (error) {
+                console.log("error", error);
+            }
+        };
+
+        fetchProfile();
+    });
     const handlePostSubmit = () => {
         const postData = {
             userId,
@@ -47,7 +65,7 @@ const ThreadsScreen = () => {
                     }}
                 />
 
-                <Text>NamPTIT</Text>
+                <Text>{user?.name}</Text>
             </View>
 
             <View style={{ flexDirection: "row", marginLeft: 10 }}>
